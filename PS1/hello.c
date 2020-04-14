4d7e777bd5f4ea5dcae145c04b6f35c349e0dc9f
hngaston <63380433+hngaston@users.noreply.github.com>
2020-04-14 11:13:50 -0400

    Update hello.c

--- PS1/hello.c
+++ PS1/hello.c
@@ -1,1 +1,10 @@
+#include <stdio.h>
+#include <cs50.h>
 
+int main(void)
+{
+    // Get variable
+    string name = get_string("What is your name\n");
+    // Execute variable
+    printf("hello, %s\n", name);
+}
