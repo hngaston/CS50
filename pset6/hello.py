75bfd00393c7985be47f53d0b01727a9fd5ffdb6
hngaston <63380433+hngaston@users.noreply.github.com>
2021-07-16 19:27:00 -0400

    Update hello.py

--- pset6/hello.py
+++ pset6/hello.py
@@ -1,9 +1,9 @@
-#hello so-and-so
+# hello so-and-so
 
 from cs50 import get_string
 
-#prompt user for input
+# prompt user for input
 answer = get_string("What is your name? ")
 
-#print hello + answer
+# print hello + answer
 print(f"hello, {answer}")
